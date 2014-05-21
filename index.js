645408b017c44931c5362ae4eaa7c9e7137e569d
Jiahui Hu <jiahui.cs@gmail.com>
2014-05-21 10:01:08 -0400

    Implement Middleware Stack

--- index.js
+++ index.js
@@ -2,11 +2,36 @@ var http=require('http');
 
 var myexpress=function(){
 	var app=function(req, res){
-		res.statusCode=404;
-		res.end();
+		app.handle(req, res);
 	}
 	app.stack=[];
-	app.use = function(fun){app.stack.push(fun)};
+
+	app.handle = function (req, res){
+		var index=0;
+		
+		var next = function(){
+			var middleware=app.stack[index++];
+			if (!middleware) {
+				res.statusCode=404;
+				res.end('404 - Not Found');
+				return;
+			}
+			middleware(req, res, next);
+		}
+
+		next();
+
+	}
+
+	app.use = function(fun){
+		
+		app.stack.push(fun);
+
+	}
+
+
+	
+
 	var server=http.createServer(app);
 	app.listen=function(port, done){
 		return server.listen(port, done);
