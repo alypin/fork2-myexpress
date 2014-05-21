d998ef1febf25b4d2e70bc8b1a20a02c8d3eafdc
Jiahui Hu <jiahui.cs@gmail.com>
2014-05-21 14:41:45 -0400

    Implement Error Handling

--- index.js
+++ index.js
@@ -9,14 +9,37 @@ var myexpress=function(){
 	app.handle = function (req, res){
 		var index=0;
 		
-		var next = function(){
+		var next = function(error){
+			
 			var middleware=app.stack[index++];
 			if (!middleware) {
+				if (error){
+				res.statusCode=500;
+				res.end();
+				
+			}
 				res.statusCode=404;
 				res.end('404 - Not Found');
 				return;
 			}
-			middleware(req, res, next);
+			try{
+				if (middleware.length <4){
+					middleware(req, res, next);
+				}
+				if(error){
+					if (middleware.length==4){
+						middleware(error, req, res, next);
+					}
+					else{
+						next(error);
+					}
+				}
+				else{
+					next();
+				}
+			}catch(error){
+				next(error);
+			}
 		}
 
 		next();
@@ -28,14 +51,14 @@ var myexpress=function(){
 		app.stack.push(fun);
 
 	}
-
-
 	
-
 	var server=http.createServer(app);
 	app.listen=function(port, done){
 		return server.listen(port, done);
 	}
+
+	
+
 	return app;
 }
 
