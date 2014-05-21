9b7388e297006b9b75d6256c44e30ff8e311bdde
Jiahui Hu <jiahui.cs@gmail.com>
2014-05-20 23:31:01 -0400

    L4, Implement app.use

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var myexpress=function(){
 		res.statusCode=404;
 		res.end();
 	}
+	app.stack=[];
+	app.use = function(fun){app.stack.push(fun)};
 	var server=http.createServer(app);
 	app.listen=function(port, done){
 		return server.listen(port, done);
